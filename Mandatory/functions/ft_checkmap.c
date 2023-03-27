4dbaa1bee28f4424afc9d461714c7e465e6274a0
Marouane Mounib <mamounib@e2r7p10.1337.ma>
2023-03-27 10:26:29 +0000

    todolist

--- Mandatory/functions/ft_checkmap.c
+++ Mandatory/functions/ft_checkmap.c
@@ -6,7 +6,7 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 11:23:53 by mamounib          #+#    #+#             */
-/*   Updated: 2023/03/27 08:39:49 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/03/27 10:25:52 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,16 @@
 int	ft_checkmap(char *filename)
 {
 
-	// TODO	: check the mape file .
-	// TODO : check the contents of the map file .
-	
+	// TODO :
+	/**
+	 * TODO	: check the mape file.
+	 * - [] check if the map is exist
+	 * - [] check if the map end by ".ber"
+	 * - [] check if the map have a name
+	 * TODO	: check the contents of the map file.
+	 * - []
+	 * - []
+	 */
 	int	fd;
 
 	fd = open(filename,O_RDONLY);
