d2fb690c1e209e6ae4e712bd4a80bd8938f1b175
Marouane Mounib <mamounib@e2r9p11.1337.ma>
2023-03-29 16:54:17 +0000

    prepare makefile and main.h

--- Makefile
+++ Makefile
@@ -5,3 +5,12 @@ CC = cc
 CFLAGS = -Wall -Wextra -Werror
 AR = ar rc
 RM = rm -f
+
+FUNCTION = Mandatory/program/main \
+		   Mandatory/functions/ft_checkmap\
+		   Mandatory/functions/ft_parce\
+		   Mandatory/functions/utils/ft_memcmp\
+		   Mandatory/functions/utils/ft_memcpy\
+		   Mandatory/functions/utils/ft_strjoin\
+		   Mandatory/functions/utils/ft_split\
+		   Mandatory/p
--- Mandatory/program/main.h
+++ Mandatory/program/main.h
@@ -6,7 +6,7 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 14:26:39 by mamounib          #+#    #+#             */
-/*   Updated: 2023/03/29 10:53:16 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/03/29 16:02:56 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,17 @@
 # define MAIN_H
 
 # include "../functions/getnextline/get_next_line.h"
+# include "../functions/printf/ft_printf.h"
 # include <stdio.h>
 # include <mlx.h>
-int ft_checkmap(char *filename);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
+typedef struct  s_map_entry{
+    int fd;
+    int len;
+    char *extention;
+    char *name;
+} t_map_entry;
 
+void	ft_checkmap(char *filename, t_map_entry *map);
+int	    ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
 #endif
