ca0909962b3cb9e22b5a2b503d9ed528c6c0d2f4
Marouane Mounib <mamounib@e2r9p11.1337.ma>
2023-03-29 16:52:44 +0000

    do it a void function and chek the file

--- Mandatory/functions/ft_checkmap.c
+++ Mandatory/functions/ft_checkmap.c
@@ -6,64 +6,41 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 11:23:53 by mamounib          #+#    #+#             */
-/*   Updated: 2023/03/29 11:07:23 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/03/29 15:21:26 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../program/main.h"
 
-int	ft_checkmap(char *filename)
+/**
+ * TODO	: check the map file.
+ * - [x] check if the map is exist
+ * - [x] check if the map end by ".ber"
+ * - [x] check if the map have a name (map.bre valid) (.ber not valid) (/de/p/.ber  not valid) (/de/p/ .ber  not valid)
+ */
+void	ft_printerror(char *error)
 {
-
-	/**
-	 * TODO	: check the mape file.
-	 * - [x] check if the map is exist
-	 * - [x] check if the map end by ".ber"
-	 * - [x] check if the map have a name (map.bre valid) (.ber not valid) (/de/p/.ber  not valid)
-	 * TODO	: check the contents of the map file.
-	 * - []
-	 * - []
-	 */
-	int	fd;
-	int	len;
-
-	fd = open(filename,O_RDONLY);
-	if (fd < 0)
-	{
-		printf("file not found: %s\n",filename);
-	}
-	else
-	{
-		len = ft_strlen(filename);
-		char	*extention;
-
-		extention = malloc(sizeof(char) * 4);
-		extention[0] = filename[len - 4];
-		extention[1] = filename[len - 3];
-		extention[2] = filename[len - 2];
-		extention[3] = filename[len - 1];
-		printf("file found: %s\n",extention);
-		printf("file found: %s\n",extention);
-		if (ft_memcmp(extention,".ber",4) == 0 && len > 4)
-		{
-			if(filename[len - 5] == '.' || filename[len - 5]== '/')
-			{
-				printf("file named .ber is not for map should hab an extantion .ber: %s\n",filename);
-				close(fd);
-				return (1);
-			}
-			else
-				printf("file found");
-		}
-		else
-			printf("dont habe .ber");
-		free(extention);
-	}
-	return 0;
+	printf("%s \n", error);
+	exit(1);
 }
 
-int main(int argc, char  **argv)
+void	ft_checkmap(char *filename, t_map_entry *map)
 {
-	ft_checkmap(argv[1]);
-	return 0;
+	map->fd = open(filename,O_RDONLY);
+	if (map->fd < 0)
+		ft_printerror("file not found!");
+	map->len = ft_strlen(filename);
+	if(map->len > 4)
+	{	
+		map->extention = ft_substr(filename, map->len - 4, 4);
+		if (ft_memcmp(map->extention,".ber",4) == 0)
+		{
+			if(filename[map->len - 5] == '.' || filename[map->len - 5] == '/' 
+				|| filename[map->len - 5] == ' ')
+				ft_printerror("bad file name!");
+		}
+		else
+			ft_printerror("bad extension!");
+		free(map->extention);
+	}
 }
