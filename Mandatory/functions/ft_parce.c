6f5ebd6a8c3707e1f62e09c8ae3ca2f3fc99f4f4
Marouane Mounib <mamounib@e2r3p3.1337.ma>
2023-03-31 16:48:08 +0000

    start cheking content of map

--- Mandatory/functions/ft_parce.c
+++ Mandatory/functions/ft_parce.c
@@ -6,7 +6,7 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 14:34:33 by mamounib          #+#    #+#             */
-/*   Updated: 2023/03/29 16:42:19 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/03/30 14:50:26 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,67 @@
 
 /**
  * TODO	: check the contents of the map file.
- * - [] check if the file is empty
- * - [] check if the ma
+ * - [x] check if the file is empty
+ * - [x] check if the new lines
+ * - [x] check if the walls
  * - [] check if the
  */
-    
-void    ft_parce(int fd, t_map_entry *map)
+
+void	ft_check_walls(char **map, int height, int with)
 {
-    char    *line;
-    char    *str;
-    
-    line = get_next_line(fd);
-    while (line)
-    {
-        str =  ft_strjoin(str, line);
-        free(line);
-        line = get_next_line(fd);
-    }
-    ft_printf("%s\n", str);
+	int	col;
+	int lin;
+	
+	height =  height -1;
+	with =  with - 1;
+	lin = 0;
+	while (lin <= height)
+	{
+		if (lin == 0 || lin == height)
+		{
+			col = 0;
+			while (col <= with)
+			{
+				if( map[lin][col] != '1')
+					ft_printerror("map Error");	
+				col++;
+			}
+		}
+		else if(map[lin][0] != '1' && map[lin][with] != '1')
+			ft_printerror("map Error");
+		lin++;
+	}
+}
+void	ft_parce(int fd, t_map_entry *map)
+{
+	char    *line;
+	char    *str;
+	int     len;
+	
+	line = get_next_line(fd);
+	map->with = ft_strlen(line);
+	if (map->with != 0)
+		map->height = 1;
+	while (line)
+	{
+		str =  ft_strjoin(str, line);
+		free(line);
+		line = get_next_line(fd);
+		map->height++;
+	}
+	len = ft_strlen(str);
+	if (len == 0 && str[len - 1] == '\n' && str[0] == '\n')
+		ft_printerror("map content invalid");
+	map->content = ft_split(str, '\n');
+	ft_check_walls(map->content, map->height, map->with);
+	ft_putstr(str);
+	
 }
 
 int main(int argc, char const *argv[])
 {
-    
-    return 0;
+	t_map_entry *map;
+	int fd = open("Mandatory/map/map.ber", O_RDONLY);
+	ft_parce(fd, map);
+	return 0;
 }
