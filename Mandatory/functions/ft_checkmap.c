fe7459a3b8ac98bfd0de14d7ad416671e618124f
Marouane Mounib <mamounib@e2r3p3.1337.ma>
2023-03-31 16:47:32 +0000

    finish the cheking map file

--- Mandatory/functions/ft_checkmap.c
+++ Mandatory/functions/ft_checkmap.c
@@ -6,7 +6,7 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 11:23:53 by mamounib          #+#    #+#             */
-/*   Updated: 2023/03/29 15:21:26 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/03/30 09:22:28 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,21 @@
  * - [x] check if the map end by ".ber"
  * - [x] check if the map have a name (map.bre valid) (.ber not valid) (/de/p/.ber  not valid) (/de/p/ .ber  not valid)
  */
-void	ft_printerror(char *error)
-{
-	printf("%s \n", error);
-	exit(1);
-}
+
 
 void	ft_checkmap(char *filename, t_map_entry *map)
 {
 	map->fd = open(filename,O_RDONLY);
 	if (map->fd < 0)
 		ft_printerror("file not found!");
-	map->len = ft_strlen(filename);
-	if(map->len > 4)
+	map->nlen = ft_strlen(filename);
+	if(map->nlen > 4)
 	{	
-		map->extention = ft_substr(filename, map->len - 4, 4);
+		map->extention = ft_substr(filename, map->nlen - 4, 4);
 		if (ft_memcmp(map->extention,".ber",4) == 0)
 		{
-			if(filename[map->len - 5] == '.' || filename[map->len - 5] == '/' 
-				|| filename[map->len - 5] == ' ')
+			if(filename[map->nlen - 5] == '.' || filename[map->nlen - 5] == '/' 
+				|| filename[map->nlen - 5] == ' ')
 				ft_printerror("bad file name!");
 		}
 		else
