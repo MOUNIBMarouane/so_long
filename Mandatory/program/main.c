7da863d8e3f348feb632510296d79983a4a83e2c
Marouane Mounib <mamounib@e2r8p11.1337.ma>
2023-04-12 05:50:40 +0000

    retry for main

--- Mandatory/program/main.c
+++ Mandatory/program/main.c
@@ -6,7 +6,7 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 08:04:59 by mamounib          #+#    #+#             */
-/*   Updated: 2023/04/07 10:24:59 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/04/12 05:46:21 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,5 @@ int	main(int argc, char **argv)
 	int			i;
 	char		*line;
 
-	(void)argc;
-	i = 0;
-	fd = ft_checkmap(argv[1], &map);
-	printf(" extantent :%s\n",map.extention);
-	ft_get_dimension(fd, &map);
-	map.content = (char **)malloc(sizeof(char *) * map.height + 1);
-	if (!map.content)
-		return (1);
-	i = 0;
-	while (i < map.height)
-	{
-		line = get_next_line(fd);
-		printf("%s\n",map.content[i]);
-
-		i++;
-	}
-	map.content[i] = NULL;
-	ft_getmap(fd, map.height);
-	printf("fd :%d\n",map.fd);
-	printf("with :%d\n",map.width);
-	printf("height :%d\n",map.height);
-	while (i < map.height)
-	{
-		printf("%s\n",map.content[i]);
-		i++;
-	}
+	
 }
