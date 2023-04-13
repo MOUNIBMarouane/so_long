dde4dae7a290ed52db7527759e70a1995bcf8591
Marouane Mounib <mamounib@e1r3p4.1337.ma>
2023-04-13 10:56:33 +0000

    add variabels for check values in map in stract

--- Mandatory/program/main.h
+++ Mandatory/program/main.h
@@ -6,7 +6,7 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 14:26:39 by mamounib          #+#    #+#             */
-/*   Updated: 2023/04/07 10:23:31 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/04/13 07:01:53 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,24 @@ typedef struct s_map_entry{
 	int		width;
 	int		height;
 	int		nlen;
+	int		n_c;
+	int		n_p;
+	int		n_e;
+	char	*name;
 	char	*extention;
 	char	**content;
 }	t_map_entry;
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_putchr(int c);
 int		ft_putstr(char *s);
-int		ft_checkmap(char *filename, t_map_entry *map);
 char	*ft_strjoin(char *s1, char *s2);
 char	**ft_split(char const *s, char c);
-char	**ft_getmap(int fd, int height);
-void	ft_parce(int fd, t_map_entry *map);
+void	ft_getmap(t_map_entry *map);
+void	ft_checkmap(t_map_entry *map);
+void	ft_cheker(t_map_entry *map);
 void	ft_printerror(char *error);
-void	ft_get_dimension(int fd, t_map_entry *map);
+void	ft_get_dimension(t_map_entry *map);
+void	ft_default_stract(t_map_entry *map);
 #endif
