7838b863732e70201789e7322a7940dadb11fc96
Marouane Mounib <mamounib@e1r3p4.1337.ma>
2023-04-13 10:54:16 +0000

    complite checking map file

--- Mandatory/functions/ft_checkmap.c
+++ Mandatory/functions/ft_checkmap.c
@@ -6,7 +6,7 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 11:23:53 by mamounib          #+#    #+#             */
-/*   Updated: 2023/04/12 06:05:58 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/04/12 10:21:33 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,22 @@
  * - [x] check if the map have a name (map.bre valid) (.ber not valid) (/de/p/.ber  not valid) (/de/p/ .ber  not valid)
  */
 
-
-int	ft_checkmap(char *filename, t_map_entry *map)
+void	ft_checkmap(t_map_entry *map)
 {
-	map->fd = open(filename,O_RDONLY);
+	map->fd = open(map->name,O_RDONLY);
 	if (map->fd < 0)
 		ft_printerror("file not found!");
-	map->nlen = ft_strlen(filename);
+	map->nlen = ft_strlen(map->name);
 	if (map->nlen > 4)
 	{	
-		map->extention = ft_substr(filename, map->nlen - 4, 4);
+		map->extention = ft_substr(map->name, map->nlen - 4, 4);
 		if (ft_memcmp(map->extention,".ber",4) == 0)
 		{
-			if (filename[map->nlen - 5] == '.' || filename[map->nlen - 5] == '/'
-				|| filename[map->nlen - 5] == ' ')
+			if (map->name[map->nlen - 5] == '.'
+				|| map->name[map->nlen - 5] == '/')
 				ft_printerror("bad file name!");
 		}
 		else
 			ft_printerror("bad extension!");
-		free(map->extention);
 	}
-	return (map->fd);
 }
