57803b6d0f7a695e1714d607a56d1b2e4c934b4b
Marouane Mounib <mamounib@e2r7p10.1337.ma>
2023-04-29 09:46:57 +0100

    add cheking path

--- Mandatory/functions/ft_check_path.c
+++ Mandatory/functions/ft_check_path.c
@@ -6,13 +6,13 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/18 06:19:40 by mamounib          #+#    #+#             */
-/*   Updated: 2023/04/18 21:48:20 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/04/29 07:59:57 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../program/main.h"
 
-char	**dupmap(char **map, int height, int width)
+char	**dupmap(char **map, int height)
 {
 	char	**map2;
 	int		i;
@@ -28,7 +28,35 @@ char	**dupmap(char **map, int height, int width)
 	return (map2);
 }
 
-void	ft_check_path(char **map)
+void	ft_flood_fill(t_map_entry *map, int x, int y)
 {
-	int	i;
+	if (map->content[y][x] == 'E'
+		|| map->content[y][x] == '1'
+		|| map->content[y][x] == 'F')
+		return ;
+	if (map->content[y][x] == 'C')
+		map->n_c++;
+	if (map->content[y][x] == 'E')
+		map->n_e++;
+	map->content[y][x] = 'F';
+	ft_flood_fill(map, x + 1, y);
+	ft_flood_fill(map, x - 1, y);
+	ft_flood_fill(map, x, y + 1);
+	ft_flood_fill(map, x, y - 1);
+}
+
+void	ft_check_path(t_map_entry map)
+{
+	t_map_entry	map2;
+
+	ft_default_map(&map2);
+	map2.content = dupmap(map.content, map.dem.h);
+	map2.dem.h = map.dem.h;
+	map2.dem.w = map.dem.w;
+	ft_chekervalues(&map2);
+	printf("in player %d %d \n", map.p_pos.x, map.p_pos.y);
+	ft_flood_fill(&map2, map.p_pos.x, map.p_pos.y);
+	ft_chekervalues(&map2);
+	if (map.n_c != map2.n_c || map.n_e != map2.n_e)
+		ft_printerror("PLAYER CANT WIN !");
 }
