33bd71cb99d4b2560fdd30c729780d49ff899d3a
Marouane Mounib <mamounib@e2r7p10.1337.ma>
2023-04-29 09:48:46 +0100

    set default values

--- Mandatory/functions/ft_default_stract.c
+++ Mandatory/functions/ft_default_stract.c
@@ -6,17 +6,29 @@
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 06:57:19 by mamounib          #+#    #+#             */
-/*   Updated: 2023/04/13 11:18:30 by mamounib         ###   ########.fr       */
+/*   Updated: 2023/04/27 10:41:13 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../program/main.h"
 
-void	ft_default_stract(t_map_entry *map)
+void	ft_default_mlx(t_mlx *mlx)
+{
+	t_dimension dem;
+
+	dem.w = 60;
+	dem.h = 60;
+	mlx->img_w = mlx_xpm_file_to_image(mlx->mlx, IMGW, &dem.w, &dem.h);
+	mlx->img_g = mlx_xpm_file_to_image(mlx->mlx, IMGG, &dem.w, &dem.h);
+	mlx->img_d = mlx_xpm_file_to_image(mlx->mlx, IMGDC, &dem.w, &dem.h);
+	mlx->img_p = mlx_xpm_file_to_image(mlx->mlx, IMGPL, &dem.w, &dem.h);
+}
+
+void	ft_default_map(t_map_entry *map)
 {
 	map->fd = -1;
-	map->width = -1;
-	map->height = -1;
+	map->dem.w = -1;
+	map->dem.h = -1;
 	map->nlen = -1;
 	map->n_c = 0;
 	map->n_p = 0;
