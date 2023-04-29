115e3d6ae5fb658dc6d7f1306597bd8587766953
Marouane Mounib <mamounib@e2r7p10.1337.ma>
2023-04-29 18:00:07 +0100

    add putnbr

--- Mandatory/functions/utils/ft_putchar_fd.c
+++ Mandatory/functions/utils/ft_putchar_fd.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/25 12:32:14 by mamounib          #+#    #+#             */
-/*   Updated: 2023/04/29 12:52:51 by mamounib         ###   ########.fr       */
+/*   Created: 2022/10/31 00:19:59 by mamounib          #+#    #+#             */
+/*   Updated: 2023/04/29 12:52:21 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../program/main.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putchar_fd(char c, int fd)
 {
-	if (n == -2147483648)
-	{
-		ft_putstr_fd("-2147483648", fd);
-		return ;
-	}
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n *= -1;
-	}
-	if (n > 9)
-		ft_putnbr_fd(n / 10, fd);
-	ft_putchar_fd(n % 10 + 48, fd);
+	write(fd, &c, 1);
 }
--- Mandatory/functions/utils/ft_putstr_fd.c
+++ Mandatory/functions/utils/ft_putstr_fd.c
@@ -1,30 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mamounib <mamounib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/25 12:32:14 by mamounib          #+#    #+#             */
-/*   Updated: 2023/04/29 12:52:51 by mamounib         ###   ########.fr       */
+/*   Created: 2022/10/23 21:41:00 by mamounib          #+#    #+#             */
+/*   Updated: 2023/04/29 12:52:13 by mamounib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../program/main.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-	if (n == -2147483648)
-	{
-		ft_putstr_fd("-2147483648", fd);
-		return ;
-	}
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n *= -1;
-	}
-	if (n > 9)
-		ft_putnbr_fd(n / 10, fd);
-	ft_putchar_fd(n % 10 + 48, fd);
+	if (s)
+		write(fd, s, ft_strlen(s));
 }
